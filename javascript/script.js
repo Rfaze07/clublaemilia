ef22a94aef1dfa7d9d98f12f40582fdf51b68b97
Rfaze07 <69684348+Rfaze07@users.noreply.github.com>
2025-05-27 20:16:54 -0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -32,6 +32,9 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+
+
+
 document.getElementById("formSocio").addEventListener("submit", function (e) {
   e.preventDefault();
   const form = new FormData(this);
@@ -53,4 +56,47 @@ document.getElementById("formContacto").addEventListener("submit", function(e) {
   const mensaje = form.get("mensaje");
   const url = `https://wa.me/5493364033688?text=Mensaje de ${nombre}:%0A${mensaje}`;
   window.open(url, "_blank");
+});
+
+function parseFecha(fechaStr) {
+  if (!fechaStr) return new Date(0); // Si está vacía, enviar al fondo
+  const partes = fechaStr.split("/");
+  if (partes.length !== 3) return new Date(0);
+  const [dia, mes, anio] = partes;
+  return new Date(`${anio}-${mes}-${dia}`); // formato ISO
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const URL_CSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7XD6wpJCLK1oTBWaDx9PixN3hOapHbFrTFuAljqDr9Iv3_myTp0AoETfCF69ZD392Ew2U5neC0YPm/pub?gid=1756030888&single=true&output=csv";
+  const container = document.getElementById("noticias-container");
+
+  Papa.parse(URL_CSV, {
+    download: true,
+    header: true,
+    complete: function (results) {
+      const datos = results.data;
+      
+      datos.sort((a, b) => parseFecha(b.Fecha) - parseFecha(a.Fecha));
+      
+
+      datos.forEach((fila) => {
+        const titulo = fila["Titulo"]?.trim();
+        const fecha = fila["Fecha"]?.trim();
+        const imagen = fila["Imagen"]?.trim();
+        const contenido = fila["Contenido"]?.trim();
+
+        if (titulo && contenido) {
+          const noticia = document.createElement("div");
+          noticia.className = "noticia-card";
+          noticia.innerHTML = `
+            ${imagen ? `<img src="${imagen}" alt="Imagen de la noticia">` : ""}
+            <h3>${titulo}</h3>
+            <div class="fecha">${fecha || ""}</div>
+            <p>${contenido.replace(/\n/g, "<br>")}</p>
+          `;
+          container.appendChild(noticia);
+        }
+      });
+    },
+  });
 });
